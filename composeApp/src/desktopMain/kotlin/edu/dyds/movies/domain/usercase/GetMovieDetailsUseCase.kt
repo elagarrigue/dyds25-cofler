008c46b2754ea28fa6c2dd6ccedbd036568db481
Lautaro Emalhao <titanwaynejuegos@gmail.com>
2025-06-03 14:31:16 -0300

    feat(usecase): implement GetMovieDetailsUseCase for fetching movie details

---
+++ composeApp/src/desktopMain/kotlin/edu/dyds/movies/domain/usercase/GetMovieDetailsUseCase.kt
@@ -0,0 +1,14 @@
+package edu.dyds.movies.domain.usecase
+
+import edu.dyds.movies.domain.entity.Movie
+import edu.dyds.movies.domain.repository.MoviesRepository
+
+interface GetMovieDetailsUseCase {
+    suspend fun getMovieDetails(id: Int): Movie?
+}
+
+internal class GetMovieDetailsUseCaseImpl(private val repository: MoviesRepository) : GetMovieDetailsUseCase {
+    override suspend fun getMovieDetails(id: Int) = repository.getMovieDetails(id)
+}
+
+
