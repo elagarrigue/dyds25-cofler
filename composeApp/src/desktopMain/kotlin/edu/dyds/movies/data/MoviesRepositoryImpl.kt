27069054ba3cdedd61f06c664a28f671780e8667
Cristian <sivacristian29@gmail.com>
2025-06-11 21:51:09 -0300

    Refactor: cambio de nombres

--- composeApp/src/desktopMain/kotlin/edu/dyds/movies/data/MoviesRepositoryImpl.kt
+++ composeApp/src/desktopMain/kotlin/edu/dyds/movies/data/MoviesRepositoryImpl.kt
@@ -5,14 +5,14 @@ import edu.dyds.movies.data.local.LocalMoviesSource
 import edu.dyds.movies.domain.entity.Movie
 import edu.dyds.movies.domain.repository.MoviesRepository
 
-class MoviesRepositoryImpl(private val localMovies: LocalMoviesSource, private val externalMovies: ExternalMoviesSource): MoviesRepository {
+class MoviesRepositoryImpl(private val localMoviesSource: LocalMoviesSource, private val externalMoviesSource: ExternalMoviesSource): MoviesRepository {
     override suspend fun getPopularMovies(): List<Movie> {
-        val cachedMovies = localMovies.getCachedMovies()
+        val localMovies = localMoviesSource.getMovies()
 
-        return cachedMovies.ifEmpty {
+        return localMovies.ifEmpty {
             try {
-                externalMovies.getPopularMovies().apply {
-                    localMovies.cacheMovies(this)
+                externalMoviesSource.getPopularMovies().apply {
+                    localMoviesSource.setMovies(this)
                 }
             } catch (e: Exception) {
                 emptyList()
@@ -22,7 +22,7 @@ class MoviesRepositoryImpl(private val localMovies: LocalMoviesSource, private v
 
     override suspend fun getMovieDetails(id: Int) =
         try {
-            externalMovies.getMovieDetails(id)
+            externalMoviesSource.getMovieDetails(id)
         } catch (e: Exception) {
             null
         }
--- composeApp/src/desktopMain/kotlin/edu/dyds/movies/data/local/LocalMovies.kt
+++ composeApp/src/desktopMain/kotlin/edu/dyds/movies/data/local/LocalMovies.kt
@@ -3,16 +3,16 @@ package edu.dyds.movies.data.local
 import edu.dyds.movies.domain.entity.Movie
 
 interface LocalMoviesSource {
-    fun getCachedMovies(): List<Movie>
-    fun cacheMovies(movies: List<Movie>)
+    fun getMovies(): List<Movie>
+    fun setMovies(movies: List<Movie>)
 }
 
 internal class LocalMoviesImpl : LocalMoviesSource {
     private val cacheMovies: MutableList<Movie> = mutableListOf()
 
-    override fun getCachedMovies() = cacheMovies.toList()
+    override fun getMovies() = cacheMovies.toList()
 
-    override fun cacheMovies(movies: List<Movie>) {
+    override fun setMovies(movies: List<Movie>) {
         cacheMovies.clear()
         cacheMovies.addAll(movies)
     }
