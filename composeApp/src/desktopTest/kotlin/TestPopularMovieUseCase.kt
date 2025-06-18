b9289bfef1f6f6a1f3be90106e8f964845e47091
gartnerfn <gartnerfn@gmail.com>
2025-06-17 21:42:54 -0300

    fix: descendant voteAverage and correct qualification test not passing

--- composeApp/src/desktopTest/kotlin/TestPopularMovieUseCase.kt
+++ composeApp/src/desktopTest/kotlin/TestPopularMovieUseCase.kt
@@ -1,12 +1,10 @@
 import edu.dyds.movies.domain.usecase.GetPopularMoviesUseCaseImpl
 import edu.dyds.movies.domain.entity.Movie
-import edu.dyds.movies.domain.entity.QualifiedMovie
 import edu.dyds.movies.domain.repository.MoviesRepository
 import kotlin.test.Test
 import kotlinx.coroutines.test.runTest
 
 class GetPopularMoviesUseCaseTest {
-
     private val movie1 = Movie(1, "title1", "overview", "releaseDate", "poster", "backdrop", "originalTitle", "originalLanguage", 5.0, 7.0)
     private val movie2 = Movie(2, "title2", "overview", "releaseDate", "poster", "backdrop", "originalTitle", "originalLanguage", 4.0, 5.0)
     private val movie3 = Movie(3, "title3", "overview", "releaseDate", "poster", "backdrop", "originalTitle", "originalLanguage", 6.0, 8.5)
@@ -25,16 +23,18 @@ class GetPopularMoviesUseCaseTest {
         //act
         val result = useCase.execute()
 
+        println("Result: $result")
+
         //assert
         assert(result.size == 3)
         assert(result[0].movie == movie3) // Mayor voto
         assert(result[0].isGoodMovie)
-        assert(!result[1].isGoodMovie) // movie2 con 5.0
-        assert(result[2].isGoodMovie)
+        assert(result[1].isGoodMovie)
+        assert(!result[2].isGoodMovie) // movie2 con 5.0
     }
 
     @Test
-    fun `execute debe retornar lista vacía si no hay películas`() = runTest {
+    fun `execute debe retornar lista vacia si no hay peliculas`() = runTest {
        //arrange
         val repository = FakeRepository(emptyList())
         val useCase = GetPopularMoviesUseCaseImpl(repository)
