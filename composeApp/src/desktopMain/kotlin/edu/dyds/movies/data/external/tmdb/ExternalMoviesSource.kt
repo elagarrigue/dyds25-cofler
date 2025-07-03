3102640194ee84465867495520c523cacfb5435c
Cristian <sivacristian29@gmail.com>
2025-07-03 14:15:18 -0300

    Dev: separación interfaz externalSource, implementación OMDBSource

--- composeApp/src/desktopMain/kotlin/edu/dyds/movies/data/external/tmdb/ExternalMoviesSource.kt
+++ composeApp/src/desktopMain/kotlin/edu/dyds/movies/data/external/tmdb/ExternalMoviesSource.kt
@@ -7,12 +7,16 @@ import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.request.*
 
-interface ExternalMoviesSource {
+interface ExternalPopularMoviesSource {
     suspend fun getPopularMovies(): List<Movie>
+}
+
+interface ExternalMovieDetailSource{
     suspend fun getMovieByTitle(title: String): Movie
 }
 
-internal class TMDBExternalMoviesSource(private val tmdbHttpClient: HttpClient) : ExternalMoviesSource {
+
+internal class TMDBExternalMoviesSource(private val tmdbHttpClient: HttpClient) : ExternalPopularMoviesSource, ExternalMovieDetailSource {
     override suspend fun getPopularMovies(): List<Movie> = getTMDBPopularMovies().results.map { it.toDomainMovie()}
 
     override suspend fun getMovieByTitle(title: String): Movie = getTMDBMovieDetails(title).apply{println(this)}.results.first().toDomainMovie()
@@ -22,5 +26,12 @@ internal class TMDBExternalMoviesSource(private val tmdbHttpClient: HttpClient) 
 
     private suspend fun getTMDBMovieDetails(title: String): RemoteResult =
         tmdbHttpClient.get("/3/search/movie?query=$title").body()
+}
+
+internal class OMDBExternalMoviesSource(private val omdbHttpClient: HttpClient): ExternalMovieDetailSource{
+    override suspend fun getMovieByTitle(title: String): Movie = getOMDBMovieDetails(title).apply{println(this)}.results.first().toDomainMovie()
+
+     private suspend fun getOMDBMovieDetails(title:String): RemoteResult =
+        omdbHttpClient.get("/3/discover/movie?sort_by=popularity.desc").body()
 }
 
